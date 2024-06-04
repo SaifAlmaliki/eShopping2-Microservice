569004d13c310e73747a950de0ccd430f6dc0dde
Saif Almaliki <saif.almaliki@pwc.com>
2024-06-04 12:22:54 +0200

    update UI

--- ApiGateway/Yarp.ApiGateway/obj/Debug/net8.0/Yarp.ApiGateway.AssemblyInfo.cs
+++ ApiGateway/Yarp.ApiGateway/obj/Debug/net8.0/Yarp.ApiGateway.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Yarp.ApiGateway")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e4a452a7775fc53092e73874209d80387781a06")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de75d67fc138c62158a274f2ff7e485a0985f057")]
 [assembly: System.Reflection.AssemblyProductAttribute("Yarp.ApiGateway")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Yarp.ApiGateway")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
