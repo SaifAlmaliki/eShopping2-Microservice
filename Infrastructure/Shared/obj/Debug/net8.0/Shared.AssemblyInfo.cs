0bc97b71c955cf571c84a14654e11aff369e6f8d
Saif Almaliki <saif.almaliki@pwc.com>
2024-06-04 12:25:41 +0200

    Reclocate Api Gateway

--- Infrastructure/Shared/obj/Debug/net8.0/Shared.AssemblyInfo.cs
+++ Infrastructure/Shared/obj/Debug/net8.0/Shared.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de75d67fc138c62158a274f2ff7e485a0985f057")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+569004d13c310e73747a950de0ccd430f6dc0dde")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
